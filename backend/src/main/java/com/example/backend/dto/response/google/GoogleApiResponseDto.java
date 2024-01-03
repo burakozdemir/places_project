c35dbd352ab2f587b9e4f183121d1325b38a0b40
Burak Özdemir <zdmr.brkk@gmail.com>
2024-01-03 22:29:27 +0300

    gcloud backend deploy

--- backend/src/main/java/com/example/backend/dto/response/google/GoogleApiResponseDto.java
+++ backend/src/main/java/com/example/backend/dto/response/google/GoogleApiResponseDto.java
@@ -2,11 +2,13 @@ package com.example.backend.dto.response.google;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class GoogleApiResponseDto {
